b52aa8864693d3ad8775c8a05bc536c8c9e7573d
Daniel Lu <daniel1211.lu@gmail.com>
2025-02-07 11:58:32 -0500

    Modernized work modal

--- components/Sections/FeaturedWorks/WorkModal.tsx
+++ components/Sections/FeaturedWorks/WorkModal.tsx
@@ -31,6 +31,7 @@ import "swiper/css/navigation";
 import styles from "./styles.module.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Work, Works } from "config/works";
+import { fadeInUp, fadeInUpSlower, simpleOpacity, stagger } from "config/animations";
 
 const MotionButton = motion(Button);
 
@@ -91,7 +92,13 @@ const WorkModal = ({ isOpen, onClose, title }: IWorkModal) => {
       scrollBehavior="inside"
     >
       <ModalOverlay />
-      <ModalContent maxW="60vw">
+      <ModalContent 
+        as={motion.div}
+        initial="initial"
+        animate="animate"
+        variants={fadeInUp}
+        maxW="60vw"
+      >
         <ModalHeader
           display="flex"
           alignItems="center"
@@ -119,138 +126,155 @@ const WorkModal = ({ isOpen, onClose, title }: IWorkModal) => {
         <ModalBody className={styles.workModal}>
           {selectedWork && (
             <Box>
-              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+              {/* Description Section */}
+              <motion.div variants={fadeInUpSlower}>
                 <Flex justifyContent="space-between" mb={4} fontWeight="bold">
                   <Text>{selectedWork.date}</Text>
                   <Text>{selectedWork.location}</Text>
                 </Flex>
+              </motion.div>
+              <motion.div variants={fadeInUpSlower}>
                 <Flex ms={4}>
                   <ul>{formattedPoints}</ul>
                 </Flex>
               </motion.div>
-              <Flex justifyContent="center" gap={4} my={4}>
-                <MotionButton
-                  variant="outline"
-                  fontWeight="light"
-                  fontSize="md"
-                  borderRadius="5px"
-                  size="md"
-                  as="a"
-                  href={selectedWork.url1}
-                  rel="noreferrer"
-                  target="_blank"
-                  whileHover={{ 
-                    boxShadow: `0px 0px 6px 0px ${hoverBoxShadowColor}`, 
-                    scale: 1.05,
-                    transition: { duration: 0.2 },
-                  }}
-                >
-                  View on GitHub
-                </MotionButton>
-                <MotionButton
-                  variant="outline"
-                  fontWeight="light"
-                  fontSize="md"
-                  borderRadius="5px"
-                  size="md"
-                  as="a"
-                  href={selectedWork.url2}
-                  rel="noreferrer"
-                  target="_blank"
-                  whileHover={{ 
-                    boxShadow: `0px 0px 6px 0px ${hoverBoxShadowColor}`, 
-                    scale: 1.05,
-                    transition: { duration: 0.2 },
-                  }}
-                >
-                  Visit Website
-                </MotionButton>
-              </Flex>
+
+              {/* Buttons Section */}
+              <motion.div variants={fadeInUpSlower}>
+                <Flex justifyContent="center" gap={4} my={4}>
+                  <MotionButton
+                    variant="outline"
+                    fontWeight="light"
+                    fontSize="md"
+                    borderRadius="5px"
+                    size="md"
+                    as="a"
+                    href={selectedWork.url1}
+                    rel="noreferrer"
+                    target="_blank"
+                    whileHover={{ 
+                      boxShadow: `0px 0px 6px 0px ${hoverBoxShadowColor}`, 
+                      scale: 1.05,
+                      transition: { duration: 0.2 },
+                    }}
+                  >
+                    View on GitHub
+                  </MotionButton>
+                  <MotionButton
+                    variant="outline"
+                    fontWeight="light"
+                    fontSize="md"
+                    borderRadius="5px"
+                    size="md"
+                    as="a"
+                    href={selectedWork.url2}
+                    rel="noreferrer"
+                    target="_blank"
+                    whileHover={{ 
+                      boxShadow: `0px 0px 6px 0px ${hoverBoxShadowColor}`, 
+                      scale: 1.05,
+                      transition: { duration: 0.2 },
+                    }}
+                  >
+                    Visit Website
+                  </MotionButton>
+                </Flex>
+              </motion.div>
+
               <Divider
                 borderColor="#A6A6A6"
                 width="100%"
                 alignSelf={"center"}
                 mb={4}
               />
-              <Swiper
-                effect={"coverflow"}
-                grabCursor={true}
-                centeredSlides={true}
-                loop={true}
-                slidesPerView={"auto"}
-                coverflowEffect={{
-                  rotate: 0,
-                  stretch: 0,
-                  depth: 100,
-                  modifier: 2.5,
-                }}
-                pagination={{ el: ".swiper-pagination", clickable: true }}
-                navigation={{
-                  nextEl: ".swiper-button-next",
-                  prevEl: ".swiper-button-prev",
-                  clickable: true,
-                }}
-                autoplay={{ delay: 3000, disableOnInteraction: false }}
-                modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
-                className="swiper_container"
-                style={{
-                  "--swiper-navigation-color": emphasis,
-                  "--swiper-pagination-color": emphasis,
-                }}
-              >
-                {selectedWork.video != undefined && (
-                  <SwiperSlide>
-                    <iframe
-                      width="100%"
-                      height="400px"
-                      src={`https://www.youtube.com/embed/${getYouTubeID(
-                        selectedWork.video
-                      )}`}
-                      title="YouTube video player"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                    />
-                  </SwiperSlide>
-                )}
-                {selectedWork.src.map((src, index) => (
-                  <SwiperSlide key={index}>
-                    <Image
-                      src={src}
-                      alt={`Picture ${index + 1}`}
-                      maxH="400px"
-                      width="100%"
-                      objectFit="contain"
-                      flexShrink={0}
-                    />
-                  </SwiperSlide>
-                ))}
-                <div className="slider-controller">
-                  <div
-                    className="swiper-button-prev slider-arrow"
-                    style={{ color: emphasis }}
-                  ></div>
-                  <div
-                    className="swiper-button-next slider-arrow"
-                    style={{ color: emphasis }}
-                  ></div>
-                  <div
-                    className="swiper-pagination"
-                    style={{ color: emphasis }}
-                  ></div>
-                </div>
-              </Swiper>
+
+              {/* Media Section */}
+              <motion.div variants={fadeInUpSlower}>
+                <Swiper
+                  effect={"coverflow"}
+                  grabCursor={true}
+                  centeredSlides={true}
+                  loop={true}
+                  slidesPerView={"auto"}
+                  coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5,
+                  }}
+                  pagination={{ el: ".swiper-pagination", clickable: true }}
+                  navigation={{
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                    clickable: true,
+                  }}
+                  autoplay={{ delay: 3000, disableOnInteraction: false }}
+                  modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+                  className="swiper_container"
+                  style={{
+                    "--swiper-navigation-color": emphasis,
+                    "--swiper-pagination-color": emphasis,
+                  }}
+                >
+                  {selectedWork.video != undefined && (
+                    <SwiperSlide>
+                      <iframe
+                        width="100%"
+                        height="400px"
+                        src={`https://www.youtube.com/embed/${getYouTubeID(
+                          selectedWork.video
+                        )}`}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      />
+                    </SwiperSlide>
+                  )}
+                  {selectedWork.src.map((src, index) => (
+                    <SwiperSlide key={index}>
+                      <Image
+                        src={src}
+                        alt={`Picture ${index + 1}`}
+                        maxH="400px"
+                        width="100%"
+                        objectFit="contain"
+                        flexShrink={0}
+                      />
+                    </SwiperSlide>
+                  ))}
+                  <div className="slider-controller">
+                    <div
+                      className="swiper-button-prev slider-arrow"
+                      style={{ color: emphasis }}
+                    ></div>
+                    <div
+                      className="swiper-button-next slider-arrow"
+                      style={{ color: emphasis }}
+                    ></div>
+                    <div
+                      className="swiper-pagination"
+                      style={{ color: emphasis }}
+                    ></div>
+                  </div>
+                </Swiper>
+              </motion.div>
+
               <Divider
                 borderColor="#A6A6A6"
                 width="100%"
                 alignSelf={"center"}
                 mt={4}
               />
-              <Text my={4} fontWeight="bold">
-                Technologies:
-              </Text>
-              <Box>
-                {renderTechnologies()}
-              </Box>
+
+              {/* Technologies Section */}
+              <motion.div variants={fadeInUpSlower}>
+                <Text my={4} fontWeight="bold">
+                  Technologies:
+                </Text>
+                <Box>
+                  {renderTechnologies()}
+                </Box>
+              </motion.div>
             </Box>
           )}
         </ModalBody>
