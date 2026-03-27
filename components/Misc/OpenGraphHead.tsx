e289059753606f5159bbda4c40c1a2adf67e209d
Daniel Lu <56236512+FinityFly@users.noreply.github.com>
2026-03-27 15:36:57 -0400

    chore: clear opengraphhead clutter

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -2,16 +2,16 @@ import Head from "next/head";
 const OpenGraphHead = () => (
   <Head>
     <title> Daniel Lu</title>
-    <meta name="description" content="Daniel Lu" />
+    {/* <meta name="description" content="Daniel Lu" /> */}
     <meta property="og:title" content="Daniel Lu" />
-    <meta property="og:site_name" content="Daniellu" />
+    <meta property="og:site_name" content="Daniel Lu" />
     <meta property="og:url" content="https://daniellu.ca/" />
-    <meta
+    {/* <meta
       property="og:description"
       content="Daniel Lu"
-    />
+    /> */}
     <meta property="og:type" content="profile" />
-    <meta property="og:image" content="https://daniellu.ca/logo.png"></meta>
+    {/* <meta property="og:image" content="https://daniellu.ca/logo.png"></meta> */}
   </Head>
 );
 export default OpenGraphHead;
